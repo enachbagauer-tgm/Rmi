bc143bfa959cc46f5dc66f41f47954c78ca62ad3
Nachbagauer-tgm <32197576+Nachbagauer-tgm@users.noreply.github.com>
2017-11-23 11:18:40 +0100

    Test

--- ProxyRmi/engine/ComputeEngine.java
+++ ProxyRmi/engine/ComputeEngine.java
@@ -28,7 +28,9 @@
  * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */ 
-
+/**
+ * @author enach
+ */
 package engine;
 
 import java.rmi.RemoteException;
